14d46c9db56d43936691d7a4f25d32ac11be8737
Evgeny <860694+evgenyk@users.noreply.github.com>
2024-10-01 17:34:54 +1000

    duplicate workflow ID

---
+++ kindeSrc/environment/workflows/accessTokenTestDuplicate/userTokenWorkflow.ts
@@ -0,0 +1,22 @@
+import {hello} from "./hello"
+
+export const workflowSettings = {
+    id: 'addAccessTokenClaim',
+    trigger: 'user:tokens_generated',
+    bindings: {
+        console: {},
+        'kinde.fetch': {},
+        'kinde.idToken': {
+            resetClaims: true
+        },
+        'kinde.accessToken': {
+            resetClaims: true
+        }
+    }
+};
+
+
+export default async function handle(event: any) {
+    kinde.accessToken.setCustomClaim('hello', 'world');
+    return 'testing add user token claim';
+}
