0bfeef7d58a1e15692e0336458b4dac9f9b6ab98
Evgeny <860694+evgenyk@users.noreply.github.com>
2024-10-09 13:19:12 +1100

    Update userTokenWorkflow.ts

--- kindeSrc/environment/workflows/accessTokenTest/userTokenWorkflow.ts
+++ kindeSrc/environment/workflows/accessTokenTest/userTokenWorkflow.ts
@@ -3,6 +3,8 @@ import {hello} from "./hello"
 export const workflowSettings = {
     id: 'addAccessTokenClaim',
     trigger: 'user:tokens_generation',
+    orgs: ['org1', 'org2'],
+    
     bindings: {
         console: {},
         'kinde.fetch': {},
@@ -11,12 +13,27 @@ export const workflowSettings = {
         },
         'kinde.accessToken': {
             resetClaims: true
-        }
+        },
+        "kinde.env": {
+            "canReadSecretValue": true,
+            "requestedSecrets": ["MY_MAIN_DATABASE_PASSWORD"]
+        },
+        "kinde.managementApi": {}
     }
 };
 
 
 export default async function handle(event: any) {
+    kinde.managementApi.users.delete('kp_sfkjhfskdjhfhkjdsh');
+    kinde.fetch('https:/ffff.cococ', {
+        headers: {
+            authorization: '${{MY_MAIN_DATABASE_PASSWORD)}'
+        }
+    })
+    const hello = kinde.env.get('MY_MAIN_DATABASE_PASSWORD');
+    console.log(hello);
+    
+    const hello = kinde.env.DEBUGGING_LEVEL;
     console.log('testing console log', {log: 'object'});
     throw new RuntimeException("Test runtime exception");
     kinde.accessToken.setCustomClaim('hello', 'world');
